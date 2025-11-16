3d697e67c1c0d4a53a31075d0d7a2187b523cbae
TC Wang <wangtct@hotmail.com>
2025-11-16 16:44:21 +0800

    s28

--- test_imports.py
+++ test_imports.py
@@ -1,1 +1,23 @@
+from dotenv import load_dotenv
+from langchain import hub 
+from langchain.agents import create_react_agent
+from langchain.agents import AgentExecutor
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+ 
+ 
 
+ 
+ 
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4", temperature=0)
+ 
+react_prompt= hub.pull("hwchase17/react")
+
+
+def main():
+  print("Hello from langchain-course")
+ 
+ 
+if __name__ == "__main__":
+    main()
